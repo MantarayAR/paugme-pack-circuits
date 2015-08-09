8c18655e4b72450c4de5eb147a21b018a2eb61f8
Ivan Montiel <idmontie@asu.edu>
2015-08-09 15:31:37 -0700

    Document logic handler

--- framework/handlers/logic-handler.js
+++ framework/handlers/logic-handler.js
@@ -1,10 +1,22 @@
-
+/**
+ * Class that handles determining
+ * voltage to boolean and boolean
+ * to voltage calculations.
+ *
+ * The Hanlder implements the Gate
+ * class in order to use the
+ * TRUE_VOLTAGE and FALSE_VOLTAGE
+ * constants.
+ *
+ * @implements Handler
+ * @implements Gate
+ */
 function LogicHandler() {
   this.Class.implement( Handler, this );
   this.Class.implement( Gate, this );
 
-  this.TRUE_VOLTAGE_FACTOR    = 2;
-  this.TRUE_VOLTAGE_THRESHOLD = this.TRUE_VOLTAGE / this.TRUE_VOLTAGE_FACTOR;
+  this.TRUE_VOLTAGE_FACTOR    = 0.5;
+  this.TRUE_VOLTAGE_THRESHOLD = this.TRUE_VOLTAGE * this.TRUE_VOLTAGE_FACTOR;
   this.VOLTAGE_TOLERANCE      = Math.pow( 10, -20 );
 
   this.toBoolean = function ( voltage ) {
