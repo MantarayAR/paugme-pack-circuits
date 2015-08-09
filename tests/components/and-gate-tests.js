a372e596660d4e431d9c5ca4f7dab6305f0c85d8
Ivan Montiel <idmontie@asu.edu>
2015-08-09 15:22:16 -0700

    Fix and gate tests

--- tests/components/and-gate-tests.js
+++ tests/components/and-gate-tests.js
@@ -3,35 +3,41 @@
 
   this.setup = function () {
     this.trueMock = new TrueLogicMockComponent();
-    this.falseMock = new TrueLogicMockComponent();
+    this.falseMock = new FalseLogicMockComponent();
+  }
+
+  this.testAndGateReturnsTrueWhenNoInputsGiven = function () {
+    var andGate = new AndGate();
+
+    this.assertValueEqualsExpected( andGate.calculate(), 5, 'The and gate should return true when no inputs given.' );
   }
 
   this.testAndGateReturnsTrue = function () {
     var andGate = new AndGate();
 
-    andGate.addInput( trueMock );
-    andGate.addInput( trueMock );
+    andGate.addInput( this.trueMock );
+    andGate.addInput( this.trueMock );
 
-    this.assertValueEqualsExpected( andGate.calculate(), 5, 'The and gate should return true' );
+    this.assertValueEqualsExpected( andGate.calculate(), 5, 'The and gate should return true.' );
   }
 
   this.testAndGateReturnsTrueWithManyInput = function () {
     var andGate = new AndGate();
 
-    andGate.addInput( trueMock );
-    andGate.addInput( trueMock );
-    andGate.addInput( trueMock );
-    andGate.addInput( trueMock );
+    andGate.addInput( this.trueMock );
+    andGate.addInput( this.trueMock );
+    andGate.addInput( this.trueMock );
+    andGate.addInput( this.trueMock );
 
-    this.assertValueEqualsExpected( andGate.calculate(), 5, 'The and gate should return true' );
+    this.assertValueEqualsExpected( andGate.calculate(), 5, 'The and gate should return true.' );
   }
 
   this.testAndGateReturnsFalse = function () {
     var andGate = new AndGate();
 
-    andGate.addInput( trueMock );
-    andGate.addInput( falseMock );
+    andGate.addInput( this.trueMock );
+    andGate.addInput( this.falseMock );
 
-    this.assertValueEqualsExpected( andGate.calculate(), 0, 'The and gate should return false' );
+    this.assertValueEqualsExpected( andGate.calculate(), 0, 'The and gate should return false.' );
   }
 }();
