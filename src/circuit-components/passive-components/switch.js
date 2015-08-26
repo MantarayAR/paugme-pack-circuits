6fa989753a02c2f01014196541dacb408358b103
Ivan Montiel <idmontie@asu.edu>
2015-08-25 21:54:55 -0700

    Add switch

---
+++ src/circuit-components/passive-components/switch.js
@@ -0,0 +1,34 @@
+var Class     = require('../../../framework/class/class.js');
+var Component = require('../../../framework/components/component.js');
+var Gate      = require('../../../framework/components/gate.js');
+
+module.exports = function Switch() {
+  this.Class.extend( Component, this );
+  this.Class.implement( Gate, this );
+
+  this.state = false;
+
+  this.turnOn = function () {
+    this.state = true;
+  }
+
+  this.turnOff = function () {
+    this.state = false;
+  }
+
+  this.toggle = function () {
+    this.state = ! this.state;
+  }
+
+  this.calculate = function () {
+    if ( this.state ) {
+      var results = [];
+      for ( var i = 0; i < this.inputs.length; i++ ) {
+        results.push( this.inputs[i].calculate() );
+      }
+      return results[0];
+    } else {
+      return this.FALSE_VOLTAGE;
+    }
+  }
+}
