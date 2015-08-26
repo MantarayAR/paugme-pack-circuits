be1bb80e693edc11ee0de4f5064009166fe446ab
Ivan Montiel <idmontie@asu.edu>
2015-08-25 20:59:24 -0700

    Add nor gate

---
+++ src/circuit-components/nor-gate.js
@@ -0,0 +1,23 @@
+var Class        = require('../../framework/class/class.js');
+var Component    = require('../../framework/components/component.js');
+var Gate         = require('../../framework/components/gate.js');
+var LogicHandler = require('../handlers/logic-handler.js');
+
+module.exports = function NorGate() {
+  this.Class.extend( Component, this );
+  this.Class.implement( Gate, this );
+
+  this.logicHandler = new LogicHandler();
+
+  this.calculate = function () {
+    var value = 0;
+
+    for ( var i = 0; i < this.inputs.length; i++ ) {
+      value += this.inputs[i].calculate();
+    }
+
+    value = this.logicHandler.toBoolean( value );
+
+    return this.logicHandler.toVoltage( ! value );
+  }
+}
---
+++ tests/components/nor-gate-test.js
@@ -0,0 +1,104 @@
+var MockInputs              = require('../mocks/mock-inputs');
+var TrueLogicMockComponent  = MockInputs.TrueLogicMockComponent;
+var FalseLogicMockComponent = MockInputs.FalseLogicMockComponent;
+var NorGate                 = require('../../src/circuit-components/nor-gate');
+var AbstractMatcher         = require('../matchers/abstract-matcher');
+
+describe('a nor gate', function () {
+  var trueMock;
+  var falseMock;
+
+  beforeEach(function() {
+    trueMock  = new TrueLogicMockComponent();
+    falseMock = new FalseLogicMockComponent();
+    jasmine.addMatchers( AbstractMatcher );
+  });
+
+  it('has no abstract methods', function () {
+    var norGate = new NorGate();
+    expect( norGate ).hasNoAbstractMethods();
+  });
+
+  it('returns true when no inputs given', function () {
+    var norGate = new NorGate();
+    expect( norGate.calculate() ).toEqual( 5 ); 
+  });
+
+  it('returns values expected by its truth table', function () {
+    var norGate = new NorGate();
+    norGate.addInput( falseMock );
+    norGate.addInput( falseMock );
+    expect( norGate.calculate() ).toEqual( 5 );
+
+    norGate = new NorGate();
+    norGate.addInput( trueMock );
+    norGate.addInput( falseMock );
+    expect( norGate.calculate() ).toEqual( 0 );
+
+    norGate = new NorGate();
+    norGate.addInput( falseMock );
+    norGate.addInput( trueMock );
+    expect( norGate.calculate() ).toEqual( 0 );
+
+    norGate = new NorGate();
+    norGate.addInput( trueMock );
+    norGate.addInput( trueMock );
+    expect( norGate.calculate() ).toEqual( 0 );
+  });
+
+  it('returns true when many inputs given', function () {
+    var norGate = new NorGate();
+
+    norGate.addInput( falseMock );
+    norGate.addInput( falseMock );
+    norGate.addInput( falseMock );
+    norGate.addInput( falseMock );
+
+    expect( norGate.calculate() ).toEqual( 5 );
+  });
+
+  it('returns false when many inputs given', function () {
+    var norGate = new NorGate();
+
+    norGate.addInput( falseMock );
+    norGate.addInput( falseMock );
+    norGate.addInput( trueMock );
+
+    expect( norGate.calculate() ).toEqual( 0 );
+  });
+
+  it('returns true after resetting', function () {
+    var norGate = new NorGate();
+
+    norGate.addInput( trueMock );
+    norGate.addInput( trueMock );
+
+    norGate.reset();
+
+    expect( norGate.calculate() ).toEqual( 5 );
+  });
+
+  it('returns true after removing inputs', function () {
+    var norGate = new NorGate();
+
+    norGate.addInput( trueMock );
+    norGate.addInput( falseMock );
+    norGate.addInput( falseMock );
+
+    norGate.removeInput( 0 );
+
+    expect( norGate.calculate() ).toEqual( 5 );
+  });
+
+  it('returns false after removing inputs', function () {
+    var norGate = new NorGate();
+
+    norGate.addInput( trueMock );
+    norGate.addInput( trueMock );
+    norGate.addInput( falseMock );
+
+    norGate.removeInput( 0 );
+
+    expect( norGate.calculate() ).toEqual( 0 );
+  });
+});
