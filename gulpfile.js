12b43939b5eefd9303ad5093f97923f27a7732fd
Ivan Montiel <idmontie@asu.edu>
2015-08-25 20:51:11 -0700

    Add code coverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp    = require('gulp');
 var jasmine = require('gulp-jasmine');
+var cover   = require('gulp-coverage');
 
 var options = {
   testPaths : [
     'tests/*.js',
     'tests/**/*.js',
+  ],
+  filePaths : [
+    'framework/*.js',
+    'framework/**/*.js',
+    'src/*.js',
+    'src/**/*.js'
   ],
   jasmine : {
     includeStackTrace : true,
@@ -14,5 +21,12 @@ var options = {
 
 gulp.task('default', function () {
   return gulp.src( options.testPaths )
-             .pipe( jasmine( options.jasmine ) );
+             .pipe(cover.instrument({
+               pattern: options.filePaths,
+               debugDirectory: 'debug'
+             }))
+             .pipe( jasmine( options.jasmine ) )
+             .pipe(cover.gather())
+             .pipe(cover.format())
+             .pipe(gulp.dest('coverage'));
 });
