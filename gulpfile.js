81b6d29e8c8d4e6b853a02d9e5a7157a73622a58
Ivan Montiel <idmontie@asu.edu>
2015-09-11 23:05:51 -0700

    Add browser build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
-var gulp    = require('gulp');
-var jasmine = require('gulp-jasmine');
-var cover   = require('gulp-coverage');
+var gulp        = require('gulp');
+var rename      = require('gulp-rename');
+var runSequence = require('run-sequence');
+
+var jasmine   = require('gulp-jasmine');
+var cover     = require('gulp-coverage');
 var coveralls = require('gulp-coveralls');
+
+var browserify = require('gulp-browserify');
+var minify     = require('gulp-minify');
 
 var options = {
   testPaths : [
@@ -17,20 +23,45 @@ var options = {
   jasmine : {
     includeStackTrace : true,
     verbose: true
-  }
+  },
+  buildPath : 'build/',
+  entryPoint : 'paugme-pack-circuits'
 }
 
-gulp.task('default', function () {
+gulp.task('test', function () {
   return gulp.src( options.testPaths )
-             .pipe(cover.instrument({
-               pattern: options.filePaths,
-               debugDirectory: 'debug'
-             }))
-             .pipe( jasmine( options.jasmine ) )
-             .pipe(cover.gather())
-             .pipe(cover.format({
-               reporter: 'lcov'
-             }))
-             .pipe(coveralls())
-             .pipe(gulp.dest('coverage'));
+          .pipe(cover.instrument({
+            pattern: options.filePaths,
+            debugDirectory: 'debug'
+          }))
+          .pipe( jasmine( options.jasmine ) )
+          .pipe(cover.gather())
+          .pipe(cover.format({
+            reporter: 'lcov'
+          }))
+          .pipe(coveralls())
+          .pipe(gulp.dest('coverage'));
+});
+
+gulp.task('compile', function () {
+  return gulp.src(options.entryPoint + '.js')
+          .pipe(browserify( {
+            insertGlobals: true
+          } ))
+          .pipe(gulp.dest(options.buildPath));
+});
+
+gulp.task('minify', function () {
+  return gulp.src(options.buildPath + options.entryPoint + '.js')
+          .pipe(minify( {} ))
+          .pipe(rename(options.entryPoint + '.min.js' ))
+          .pipe(gulp.dest(options.buildPath));
+});
+
+gulp.task('build', function () {
+  runSequence('compile', 'minify');
+});
+
+gulp.task('default', function ( cb ) {
+  runSequence( 'test', 'build', cb );
 });
